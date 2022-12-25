8e7645b1dedd8224ac1012f55dfa844752cefeb8
TraceL <kivlanbahmidt@gmail.com>
2022-12-26 02:55:28 +0700

    Finalize filtering

--- components/StyledFilter.tsx
+++ components/StyledFilter.tsx
@@ -1,4 +1,4 @@
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, Switch, Transition } from "@headlessui/react";
 import { Fragment, ReactNode, useEffect, useRef, useState } from "react";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid"
 import { CheckIcon } from "@heroicons/react/24/solid"
@@ -7,6 +7,8 @@ type Props<T> = {
     selectedItems: T[];
     allItems: T[];
     setItems: (items: T[]) => void;
+    anyFilter: boolean;
+    setAnyFilter: (set: boolean) => void;
     buttonLabel: string;
 }
 
@@ -18,7 +20,7 @@ type SimpleDOMRect = {
 }
 
 export default function StyledFilter<T extends ReactNode>(props: Props<T>) {
-    const { selectedItems, allItems, setItems, buttonLabel } = props;
+    const { selectedItems, allItems, setItems, buttonLabel, anyFilter, setAnyFilter } = props;
 
     const [currButtonRect, setButtonRect] = useState<SimpleDOMRect>({
         height: 0,
@@ -65,7 +67,7 @@ export default function StyledFilter<T extends ReactNode>(props: Props<T>) {
 
     return (
         <Listbox value={selectedItems} onChange={setItems} multiple>
-            <Listbox.Button ref={buttonNode} className="flex flex-row items-center justify-between md:w-36 lg:w-56 relative m-2 p-2 bg-stone-200 rounded-xl transition-all ease-in-out hover:shadow-inner hover:bg-stone-400">
+            <Listbox.Button ref={buttonNode} className="flex flex-row items-center justify-between w-72 md:w-48 lg:w-72 relative m-2 p-2 bg-stone-200 rounded-xl transition-all ease-in-out hover:shadow-inner hover:bg-stone-400">
                 <span className="mx-2">
                     {selectedItems.length == 0 ? `${buttonLabel}...` : `${buttonLabel} (${selectedItems.length})`}
                 </span>
@@ -74,15 +76,33 @@ export default function StyledFilter<T extends ReactNode>(props: Props<T>) {
                 </span>
             </Listbox.Button>
             <Transition
-            enter="transition duration-100 ease-out"
-            enterFrom="transform scale-95 opacity-0"
-            enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
-            leaveFrom="transform scale-100 opacity-100"
-            leaveTo="transform scale-95 opacity-0"
-            as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+                as={Fragment}
             >
-                <Listbox.Options className="absolute mt-3 m-2 p-2 z-10 bg-stone-200 rounded-xl" style={getTopLeftStyle()}>
+                <Listbox.Options className="absolute mt-3 m-2 z-10 shadow-inner outline outline-2 outline-stone-200 bg-white rounded-xl" style={getTopLeftStyle()}>
+                    <div className="flex items-center justify-center">
+                        <div className="text-md tracking-tight font-light">
+                            Any
+                        </div>
+                        <Switch
+                            onChange={setAnyFilter}
+                            checked={anyFilter}
+                            className={`${anyFilter ? 'bg-blue-600' : 'bg-gray-200'
+                                }  rounded-xl w-12 h-6 mx-4 my-2 transition-colors`}
+                        >
+                            <span className={`${anyFilter ? 'translate-x-3' : '-translate-x-3'} transform-gpu transition ease-in-out rounded-full bg-stone-100 w-6 h-6 inline-block shadow-lg`}>
+
+                            </span>
+                        </Switch>
+                        <div className="text-md tracking-tight font-light">
+                            All
+                        </div>
+                    </div>
                     {allItems.map((item, idx) => (
                         <Listbox.Option
                             value={item}
@@ -90,9 +110,13 @@ export default function StyledFilter<T extends ReactNode>(props: Props<T>) {
                             as={Fragment}
                         >
                             {({ active, selected }) => (
-                                <li className={"flex flex-row items-center gap-2 py-1" + " " + (selected ? "" : "") + " " + (active ? "bg-stone-400" : "")}>
-                                    <span className="w-5 h-5">
-                                        {selected ? <CheckIcon/> : null}
+                                <li className={"flex flex-row items-center gap-2 py-1 text-sm tracking-tight transition-colors duration-75 cursor-default" + " "
+                                    + (selected ? "font-bold" : "font-light") + " "
+                                    + (active ? "bg-stone-400" : "") + " "
+                                    + (idx == allItems.length - 1 ? "rounded-b-xl" : "")}
+                                >
+                                    <span className="w-5 h-5 ml-1">
+                                        {selected ? <CheckIcon /> : null}
                                     </span>
                                     <span>{item}</span>
                                 </li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import ArtCard from '../components/ArtCard'
 import Header from '../components/Header';
 import { ImageCard, ImageCfg } from '../types/imageTypes';
-import { getDocs, limit, orderBy, query } from 'firebase/firestore';
+import { getDocs, limit, orderBy, Primitive, query } from 'firebase/firestore';
 import { getDownloadURL, ref } from 'firebase/storage';
 import { imageCol, imageBucket } from '../utils/databases';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Filter from '../components/Filter';
 
 type Props = {
@@ -13,6 +13,31 @@ type Props = {
   allCharacters: Array<string>;
   allMetas: Array<string>;
   allArtists: Array<string>;
+}
+
+function arraysEqualAll<T>(a: T[], b: T[]): boolean {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  const aCopy = [...a].sort();
+  const bCopy = [...b].sort();
+
+  for (var i = 0; i < aCopy.length; ++i) {
+    if (aCopy[i] !== bCopy[i]) return false;
+  }
+  return true;
+}
+
+function arraysEqualAny<T extends Primitive>(a: T[], b: T[]) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+
+  for (const el of a) {
+    if (b.includes(el)) return true;
+  }
+
+  return false;
 }
 
 export async function getStaticProps() {
@@ -52,10 +77,52 @@ export async function getStaticProps() {
 export default function Home(props: Props) {
   const { allImages, allArtists, allCharacters, allMetas } = props;
 
-  const [images, setImages] = useState(allImages);
+  const [images, setImages] = useState<Array<ImageCard>>(allImages)
   const [artistFilter, setArtistFilter] = useState<Array<string>>([]);
   const [characterFilter, setCharacterFilter] = useState<Array<string>>([]);
   const [metaFilter, setMetaFilter] = useState<Array<string>>([]);
+
+  const [isFilterAllArtist, setFilterAllArtist] = useState<boolean>(false);
+  const [isFilterAllChar, setFilterAllChar] = useState<boolean>(false);
+  const [isFilterAllMeta, setFilterAllMeta] = useState<boolean>(false);
+
+  function filterArtists(imagesToFilter: ImageCard[]) {
+    if (artistFilter.length == 0) return imagesToFilter;
+
+    if (isFilterAllArtist) {
+      return imagesToFilter.filter((img) => arraysEqualAll(img.artists, artistFilter));
+    }  else {
+      return imagesToFilter.filter((img) => arraysEqualAny(img.artists, artistFilter))
+    }
+  }
+
+  function filterChars(imagesToFilter: ImageCard[]) {
+    if (characterFilter.length == 0) return imagesToFilter;
+
+    if (isFilterAllChar) {
+      return imagesToFilter.filter((img) => arraysEqualAll(img.characters, characterFilter));
+    }  else {
+      return imagesToFilter.filter((img) => arraysEqualAny(img.characters, characterFilter))
+    }
+  }
+
+  function filterMetas(imagesToFilter: ImageCard[]) {
+    if (metaFilter.length == 0) return imagesToFilter;
+
+    if (isFilterAllMeta) {
+      return imagesToFilter.filter((img) => arraysEqualAll(img.meta, metaFilter));
+    }  else {
+      return imagesToFilter.filter((img) => arraysEqualAny(img.meta, metaFilter))
+    }
+  }
+
+  useEffect(() => {
+    const filteredArtists = filterArtists(allImages);
+    const filteredChars = filterChars(filteredArtists);
+    const filteredImages = filterMetas(filteredChars);
+
+    setImages(filteredImages);
+  }, [artistFilter, characterFilter, metaFilter, isFilterAllArtist, isFilterAllChar, isFilterAllMeta])
 
   return (
     <>
@@ -78,9 +145,15 @@ export default function Home(props: Props) {
         setArtist={(artist) => setArtistFilter(artist)}
         setChar={(char) => setCharacterFilter(char)}
         setMeta={(meta) => setMetaFilter(meta)}
+        filterAllArtist={isFilterAllArtist}
+        filterAllChar={isFilterAllChar}
+        filterAllMeta={isFilterAllMeta}
+        setFilterAllArtist={(bool) => setFilterAllArtist(bool)}
+        setFilterAllChar={(bool) => setFilterAllChar(bool)}
+        setFilterAllMeta={(bool) => setFilterAllMeta(bool)}
         />
         <div className='flex flex-wrap gap-4 justify-center xl:mx-24 lg:mx-14 md:mx-8 mx-4'>
-          {allImages.map((imageCard, index) => <ArtCard imgCard={imageCard} key={index} />)}
+          {images.map((imageCard, index) => <ArtCard imgCard={imageCard} key={index} />)}
         </div>
       </div>
     </>
