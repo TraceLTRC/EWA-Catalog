5f19d7b10547715b221b12649883148b9ceba6bf
TraceL <kivlanbahmidt@gmail.com>
2022-12-26 04:05:43 +0700

    fix ssr issue + dropdown issue

--- components/StyledFilter.tsx
+++ components/StyledFilter.tsx
@@ -2,8 +2,8 @@ import { Listbox, Switch, Transition } from "@headlessui/react";
 import { Fragment, ReactNode, useLayoutEffect, useRef, useState } from "react";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid"
 import { CheckIcon } from "@heroicons/react/24/solid"
-
-//TODO fix warnings due to SRR-UseLayoutEffect
+import { useFloating } from "@floating-ui/react-dom";
+import { autoUpdate, offset } from "@floating-ui/react-dom";
 
 type Props<T> = {
     selectedItems: T[];
@@ -14,59 +14,17 @@ type Props<T> = {
     buttonLabel: string;
 }
 
-type SimpleDOMRect = {
-    top: number,
-    left: number,
-    width: number,
-    height: number,
-}
-
 export default function StyledFilter<T extends ReactNode>(props: Props<T>) {
     const { selectedItems, allItems, setItems, buttonLabel, anyFilter, setAnyFilter } = props;
 
-    const [currButtonRect, setButtonRect] = useState<SimpleDOMRect>({
-        height: 0,
-        left: 0,
-        top: 0,
-        width: 0,
+    const {x, y, reference, floating, strategy} = useFloating<HTMLButtonElement>({
+        whileElementsMounted: autoUpdate,
+        middleware: [offset({crossAxis: -8, mainAxis: -8})]
     });
-    const buttonNode = useRef<HTMLButtonElement>(null);
-
-    const setButtonRectCallback = () => {
-        if (buttonNode.current) {
-            const trueRect = buttonNode.current.getBoundingClientRect();
-            const rect: SimpleDOMRect = {
-                height: trueRect.height,
-                width: trueRect.width,
-                left: trueRect.left,
-                top: trueRect.top,
-            }
-
-            setButtonRect(rect);
-        };
-    }
-
-    const getTopLeftStyle: () => React.CSSProperties = () => {
-        return {
-            top: currButtonRect.top + currButtonRect.height - 8,
-            left: currButtonRect.left - 8,
-            width: currButtonRect.width
-        }
-    }
-
-    useLayoutEffect(() => {
-        setButtonRectCallback();
-
-        window.addEventListener('resize', setButtonRectCallback);
-
-        return () => {
-            window.removeEventListener('resize', setButtonRectCallback);
-        }
-    }, [])
 
     return (
         <Listbox value={selectedItems} onChange={setItems} multiple>
-            <Listbox.Button ref={buttonNode} className="flex flex-row items-center justify-between w-72 md:w-48 lg:w-72 relative m-2 p-2 bg-stone-200 rounded-xl transition-all ease-in-out hover:shadow-inner hover:bg-stone-400">
+            <Listbox.Button ref={reference} className="flex flex-row items-center justify-between w-72 md:w-48 lg:w-72 relative m-2 p-2 bg-stone-200 rounded-xl transition-all ease-in-out hover:shadow-inner hover:bg-stone-400">
                 <span className="mx-2">
                     {selectedItems.length == 0 ? `${buttonLabel}...` : `${buttonLabel} (${selectedItems.length})`}
                 </span>
@@ -83,7 +41,13 @@ export default function StyledFilter<T extends ReactNode>(props: Props<T>) {
                 leaveTo="transform scale-95 opacity-0"
                 as={Fragment}
             >
-                <Listbox.Options className="absolute mt-3 m-2 z-10 shadow-inner outline outline-2 outline-stone-200 bg-white rounded-xl" style={getTopLeftStyle()}>
+                <Listbox.Options ref={floating} 
+                style={{
+                    position: strategy,
+                    top: y ?? 0,
+                    left: x ?? 0,
+                }}
+                className="absolute w-72 md:w-48 lg:w-72 mt-3 m-2 z-10 shadow-inner outline outline-2 outline-stone-200 bg-white rounded-xl">
                     <div className="flex items-center justify-center">
                         <div className="text-md tracking-tight font-light">
                             Any
